ec3fc4b4955ea90af07acd4f878f048c09acf304
jagtarsingh <jagtarcgc.mca11@gmail.com>
2022-01-26 18:03:21 +0530

    Count currency program

---
+++ src/com/gl/lab2/CurrencyCount.java
@@ -0,0 +1,29 @@
+package com.gl.lab2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import com.gl.lab2.MergeSortImplementation;
+import com.gl.lab2.Count;
+
+public class CurrencyCount {
+
+	public static void main(String[] args) {
+
+		MergeSortImplementation mergeSortImplementation = new MergeSortImplementation();
+		Count notesCount = new Count();
+
+		System.out.println("enter the size of currency denominations ");
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] notes = new int[size];
+		System.out.println("enter the currency denominations value");
+		for (int i = 0; i < size; i++) {
+			notes[i] = sc.nextInt();
+		}
+		System.out.println("enter the amount you want to pay");
+		int amount = sc.nextInt();
+		mergeSortImplementation.sort(notes,0,notes.length-1);
+		notesCount.totalCount(notes, amount);
+	}
+}
